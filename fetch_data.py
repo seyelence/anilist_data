c42d76f12dca54ad1f387a0e80816fa2f4318ca4
seyelence <infinityx14@gmail.com>
2023-08-17 00:27:29 -0400

    had to change implementation to get data, now i can get over 5k rows. rate limited

--- fetch_data.py
+++ fetch_data.py
@@ -2,7 +2,7 @@ import time
 import requests
 
 API_ENDPOINT = 'https://graphql.anilist.co'
-PER_PAGE = 100
+PER_PAGE = 50
 
 # GraphQL query
 query = '''
@@ -13,6 +13,7 @@ query ($page: Int, $perPage: Int) {
       total
       currentPage
       lastPage
+      hasNextPage
     }
     media(type: ANIME) {
       id
@@ -56,20 +57,24 @@ query ($page: Int, $perPage: Int) {
 anime_info_table = None
 
 def fetch_anime_data(page_num):
-    response = requests.post(API_ENDPOINT, json={'query': query, 'variables': {'page': page_num, 'perPage': PER_PAGE}})
-    return response.json()
+  response = requests.post(API_ENDPOINT, json={'query': query, 'variables': {'page': page_num, 'perPage': PER_PAGE}})
+  return response.json()
 
 def fetch_all_anime_data():
-    all_anime_details = []
-    response = fetch_anime_data(1)
-    total_anime = response['data']['Page']['pageInfo']['total']
-    last_page = response['data']['Page']['pageInfo']['lastPage']
+  all_anime_details = []
+  page = 1
+    
+  while len(all_anime_details) <= 15000:
+    response = fetch_anime_data(page)
+    anime_list = response['data']['Page']['media']
+    all_anime_details += anime_list
+    if not response['data']['Page']['pageInfo']['hasNextPage']:
+      break
+      
+    page += 1
+    time.sleep(60)
 
-    for page in range(1, last_page + 1):
-        response = fetch_anime_data(page)
-        anime_list = response['data']['Page']['media']
-        all_anime_details += anime_list
-        if page < last_page:
-            time.sleep(1)
+  return all_anime_details
 
-    return all_anime_details
+all_data = fetch_all_anime_data()
+print("Fetched total entries:", len(all_data))
